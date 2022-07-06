2283819e0ae2cc5745aad0486375801ef89ae263
Kimseonghyoek <cilo1223@naver.com>
2022-07-06 16:57:27 +0900

    Update App.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const PORT = 8081;
+const con = require('./DB/mysql');
+const loginRouter = require('./routes/loginRouter');
+const indexRouter = require('./routes/indexRouter');
 
 app.set("view engine", "html");
 app.use(express.json());
@@ -10,6 +13,10 @@ app.use(express.static('../Develuv_frontend/develuv-f/build'))
 app.get('/', (req, res) => {
   res.sendFile('index.html');
 })
+
+// app.use('/', indexRouter);
+
+app.use('/login', loginRouter);
 
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
