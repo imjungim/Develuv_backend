dca0b2186c1b5b50c5589590b0e3c780e79d662c
Kimseonghyoek <cilo1223@naver.com>
2022-07-21 03:40:58 +0900

    Update login router

--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -1,31 +1,43 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const con = require('../DB/mysql');
-const bcrybt = require('bcrypt');
+const con = require("../DB/mysql");
+const bcrybt = require("bcrypt");
+const passport = require("passport");
 
-router.get('/', (req, res) => {
-  console.log('login!');
-});
-
-router.post('/signup/post', async (req, res) => {
+router.post("/signup/post", async (req, res) => {
   const email = req.body.email.email;
   const name = req.body.nickname.nickname;
   const id = req.body.user_id;
   const pw = req.body.pw.pw;
   const hashed = await bcrybt.hash(pw, 10);
 
-  const sqlInsert = {email: email, nickname: name, user_id: id, pw: hashed}
-  con.query('select email from user_table where email=?', [email], (err, rows) => {
-    if(rows.length) {
-      res.json({'result':'fail'})
-    } else {
-      con.query('insert into user_table set?', sqlInsert, (err, rows) => {
-        if(err) throw err;
-        console.log('ok');
-        res.json({'result':'ok'});
-      })
+  const sqlInsert = { email: email, nickname: name, user_id: id, pw: hashed };
+  con.query(
+    "select email from user_table where email=?",
+    [email],
+    (err, rows) => {
+      if (rows.length) {
+        res.json({ result: "fail" });
+      } else {
+        con.query("insert into user_table set?", sqlInsert, (err, rows) => {
+          if (err) throw err;
+          console.log("ok");
+          res.json({ result: "ok" });
+        });
+      }
     }
-  })
+  );
+});
+
+// passport local 전략
+router.post("/post", (req, res) => {
+  if(res) {
+    console.log('test')
+    passport.authenticate("local", {
+      successRedirect: "/adtad",
+      failureRedirect: "/lgasdfa"
+    })
+  }
 });
 
 module.exports = router;
